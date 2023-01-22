d8e68b2ed69d18165b56da6dfcd20031d1c4d9e6
churbaeva <fedosova1997@mail.ru>
2023-01-22 15:46:38 +0500

    1

---
+++ work1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number / 10) % 10;
+System.Console.WriteLine($"{number} -> {result}");
